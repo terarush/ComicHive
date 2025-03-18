0f13f04061415d4ac30e2c76c323f0d736c95837
rafia9005 <rafia9005@gmail.com>
2025-03-18 19:07:15 +0700

    fix conflix cors

--- app/src/utils/Fetch.ts
+++ app/src/utils/Fetch.ts
@@ -5,7 +5,6 @@ export const Fetch = axios.create({
   baseURL: ENV.API_URL,
   headers: {
     "Content-Type": "application/json",
-    "Origin": ENV.CLIENT_URL,
     "X-Requested-With": "XMLHttpRequest",
   },
 });
