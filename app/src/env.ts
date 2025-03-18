78d4b3a4eba07505245d08bc8f9fb5cf1da206aa
rafia9005 <rafia9005@gmail.com>
2025-03-18 10:46:54 +0700

    change(api url): small update change api url

--- app/src/env.ts
+++ app/src/env.ts
@@ -1,3 +1,7 @@
+//export const ENV = {
+//  API_URL: import.meta.env.VITE_TEST_API_URL
+//}
+
 export const ENV = {
-  API_URL: import.meta.env.VITE_TEST_API_URL
+  API_URL: import.meta.env.VITE_API_URL
 }
