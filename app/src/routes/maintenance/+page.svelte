4ecfbe3ea565b2f5731a482264a25296a36ca909
rafia9005 <rafia9005@gmail.com>
2025-03-19 12:10:17 +0700

    505 mode

--- app/src/routes/maintenance/+page.svelte
+++ app/src/routes/maintenance/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import Hero from "../components/Hero.svelte";
-  import MangaLayout from "../components/layouts/MangaLayout.svelte";
-  import LoadingElements from "../components/elements/LoadingElements.svelte";
-  import SearchManga from "../components/SearchManga.svelte";
-  import TopManga from "../components/TopManga.svelte";
-  import { fetchPopularManga } from "../hooks/MangaHooks";
+  import Hero from "../../components/Hero.svelte";
+  import MangaLayout from "../../components/layouts/MangaLayout.svelte";
+  import LoadingElements from "../../components/elements/LoadingElements.svelte";
+  import SearchManga from "../../components/SearchManga.svelte";
+  import TopManga from "../../components/TopManga.svelte";
+  import { fetchPopularManga } from "../../hooks/MangaHooks";
 
   let mangaList: {
     id: number;
