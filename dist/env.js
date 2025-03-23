3ba4d13b1b4dc13ea1934c9de184000769589ab8
rafia9005 <rafia9005@gmail.com>
2025-03-23 23:02:13 +0700

    change dist

--- dist/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ENV = void 0;
-require("dotenv/config");
-exports.ENV = {
-    PORT: process.env.PORT || 3000,
-};
--- dist/router/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const hono_1 = require("hono");
-const auth_controller_1 = require("../controller/auth-controller");
-const r = new hono_1.Hono();
-r.route('/auth', auth_controller_1.authController);
-exports.default = r;
