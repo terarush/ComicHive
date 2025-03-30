3799ba50fd5aa532aff4e40b4a29d7efd4f2359c
rafia9005 <rafia9005@gmail.com>
2025-03-30 19:24:21 +0700

    build

--- dist/controller/comment-controller.js
+++ dist/controller/comment-controller.js
@@ -5,6 +5,22 @@ const hono_1 = require("hono");
 const middleware_1 = require("../middleware/middleware");
 const comment_service_1 = require("../service/comment-service");
 exports.commentController = new hono_1.Hono();
+exports.commentController.post("/reply", middleware_1.authMiddleware, async (c) => {
+    const userId = c.get("userId");
+    const request = (await c.req.json());
+    const response = await comment_service_1.CommentService.replyComment(userId, request);
+    return c.json({
+        data: response,
+    });
+});
+exports.commentController.delete("/reply/:replyId", middleware_1.authMiddleware, async (c) => {
+    const userId = c.get("userId");
+    const replyId = c.req.param("replyId");
+    const response = await comment_service_1.CommentService.deleteReplyComment(userId, replyId);
+    return c.json({
+        data: response,
+    });
+});
 exports.commentController.get("/:animeId", async (c) => {
     const animeId = c.req.param("animeId");
     const response = await comment_service_1.CommentService.getComment(animeId);
@@ -17,6 +33,14 @@ exports.commentController.post("/:animeId", middleware_1.authMiddleware, async (
     const animeId = c.req.param("animeId");
     const request = (await c.req.json());
     const response = await comment_service_1.CommentService.postComment(animeId, userId, request);
+    return c.json({
+        data: response,
+    });
+});
+exports.commentController.delete("/:commentId", middleware_1.authMiddleware, async (c) => {
+    const userId = c.get("userId");
+    const commentId = c.req.param("commentId");
+    const response = await comment_service_1.CommentService.deleteComment(userId, commentId);
     return c.json({
         data: response,
     });
--- dist/service/comment-service.js
+++ dist/service/comment-service.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.CommentService = void 0;
+const http_exception_1 = require("hono/http-exception");
 const database_1 = require("../application/database");
 const comment_validation_1 = require("../validation/comment-validation");
 class CommentService {
@@ -12,7 +13,9 @@ class CommentService {
             return { message: "There are no comments on this anime yet." };
         }
         const comments = await database_1.prismaClient.comment.findMany({
-            where: { animeId },
+            where: {
+                animeId,
+            },
             select: {
                 id: true,
                 content: true,
@@ -24,6 +27,21 @@ class CommentService {
                         avatar: true,
                         created_at: true,
                         updated_at: true,
+                    },
+                },
+                replies: {
+                    select: {
+                        id: true,
+                        content: true,
+                        created_at: true,
+                        user: {
+                            select: {
+                                id: true,
+                                username: true,
+                                avatar: true,
+                                created_at: true,
+                            },
+                        },
                     },
                 },
             },
@@ -46,6 +64,57 @@ class CommentService {
             },
         });
         return { message: "Success", comment };
+    }
+    static async deleteComment(userId, commentId) {
+        const comment = await database_1.prismaClient.comment.findUnique({
+            where: { id: commentId, userId },
+        });
+        if (!comment) {
+            throw new http_exception_1.HTTPException(404, { message: "Comment not found" });
+        }
+        await database_1.prismaClient.comment.delete({
+            where: {
+                id: commentId,
+            },
+        });
+        return { message: "Comment deleted" };
+    }
+    static async replyComment(userId, request) {
+        request = comment_validation_1.CommentValidation.REPLY_COMMENT.parse(request);
+        const comment = await database_1.prismaClient.comment.findUnique({
+            where: { id: request.comment_id },
+        });
+        if (!comment) {
+            throw new http_exception_1.HTTPException(404, { message: "Comment not found" });
+        }
+        const reply = await database_1.prismaClient.replyComment.create({
+            data: {
+                userId,
+                content: request.content,
+                commentId: request.comment_id,
+                created_at: new Date(),
+            },
+            select: {
+                id: true,
+                content: true,
+                created_at: true,
+            },
+        });
+        return reply;
+    }
+    static async deleteReplyComment(userId, replyId) {
+        const reply = await database_1.prismaClient.replyComment.findUnique({
+            where: { id: replyId, userId },
+        });
+        if (!reply) {
+            throw new http_exception_1.HTTPException(404, { message: "Reply not found" });
+        }
+        await database_1.prismaClient.replyComment.delete({
+            where: {
+                id: replyId,
+            },
+        });
+        return { message: "Reply deleted" };
     }
 }
 exports.CommentService = CommentService;
