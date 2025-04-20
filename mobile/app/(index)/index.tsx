d9db23f8eda3b7d3f79451eab3cc0ee7884b99a4
rafia9005 <rafia9005@gmail.com>
2025-04-20 19:21:15 +0700

    Install nativewind

---
+++ mobile/app/(index)/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function index() {
+  return (
+    <View className="flex justify-center items-center h-screen">
+      <Text className="text-white text-5xl text-red-500">
+        hello world
+      </Text>
+    </View>
+  )
+}
+
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -5,10 +5,10 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import '@/global.css'
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -30,8 +30,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+        <Stack.Screen name="(index)" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
